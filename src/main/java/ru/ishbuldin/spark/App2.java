259b825f390c2330aeb55e6585b14330418c168b
Ishbuldin Andrei <ishbuldin89@gmail.com>
2020-03-01 21:38:37 +0500

    added max and sum output

--- src/main/java/ru/ishbuldin/spark/App2.java
+++ src/main/java/ru/ishbuldin/spark/App2.java
@@ -8,7 +8,7 @@ import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
 
-import java.util.*;
+import java.util.stream.Stream;
 
 public class App2 {
     public static void main(String[] args) {
@@ -21,21 +21,22 @@ public class App2 {
                 .appName("Java Spark SQL")
                 .getOrCreate();
         String[] paths = {"D:/data/emp/hr/", "D:/data/emp/developers/", "D:/data/emp/managers/"};
-        List<String> files = new ArrayList<>(Arrays.asList(paths));
-        JavaPairRDD<String, String> javaPairRDD = sc.wholeTextFiles("D:/data/emp/hr/,D:/data/emp/developers/,D:/data/emp/managers/");
+        JavaPairRDD<String, String> javaPairRDD = sc.wholeTextFiles(Stream.of(paths).reduce((s1, s2) -> s1 + "," + s2).get());
 
         StructType structType = new StructType();
         structType = structType.add("department", DataTypes.StringType, true);
         structType = structType.add("json", DataTypes.StringType, true);
-
         StructType json = new StructType();
         json = json.add("name", DataTypes.StringType);
         json = json.add("salary", DataTypes.StringType);
 
         JavaRDD<Row> javaRDD = javaPairRDD.map(a -> RowFactory.create(getDepartment(a._1()), a._2()));
         Dataset<Row> df = spark.createDataFrame(javaRDD, structType);
-        df.withColumn("parsed_json", functions.from_json(functions.col("json"), json))
-                .select("department","parsed_json.*").show();
+        df = df.withColumn("parsed_json", functions.from_json(functions.col("json"), json))
+                .select("parsed_json.*", "department");
+        df.show();
+        df.select(functions.max(functions.col("salary").cast(DataTypes.IntegerType))).show();
+        df.select(functions.sum(functions.col("salary").cast(DataTypes.IntegerType))).show();
     }
 
     private static String getDepartment(String file) {
